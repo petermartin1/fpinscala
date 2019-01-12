65307ee3c456cf5efac5f2699924814b699b8bbe
u225660 <peter.martin@united.com>
2019-01-12 11:24:54 -0600

    Lists

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # fpinscala
+=======
+# scalaredbook
+>>>>>>> first commit
---
+++ src/main/scala/fpinscalla/datastructures/List.scala
@@ -0,0 +1,50 @@
+package fpinscalla.datastructures
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](as: List[A]): List[A] = drop(as, 1)
+
+  def setHead[A](a: A, as: List[A]): List[A] = as match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(a, t)
+  }
+
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
+    case (_, 0) => l
+    case (Nil, _) => Nil
+    case (Cons(h, t), _) => drop(t, n - 1)
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) if (f(h)) => dropWhile(t, f)
+    case _ => l
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
+  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B =
+}
